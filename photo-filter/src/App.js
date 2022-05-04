17b7e67f6e4a4c79f8bec0c976d670dceab1f168
David Howard <hotcheesesoup6@aol.com>
2022-05-04 09:52:54 -0500

    Initial commit

---
+++ photo-filter/src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './App.css';
+import Slider from './Slider'
+
+function App() {
+  return (
+    <div className="container">
+      <div className="main-image">Image</div> 
+      <div className="sidebar">
+        Sidebar
+      </div>
+      <Slider />
+    </div>
+  )
+}
+
+export default App;
